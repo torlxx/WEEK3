50c4f221b8b2b409020472be8125898de1ad0649
torlxx <116717868+torlxx@users.noreply.github.com>
2022-11-08 17:48:51 +0100

    Update UDP_Flood.py

--- UDP_Flood.py
+++ UDP_Flood.py
@@ -1,7 +1,7 @@
 #Script_in_Python_per_simulazione_UDP_Flood
 
 import socket      #modulo_sulle_socket
-
+import random      #modulo_randon_per_generazione_byte_casuali 
 SRV_ADDR = "192.168.33.100" #ip_client
 SVR_PORT = 44444            #porta_client
 
@@ -12,7 +12,7 @@ print ("Server started! Waiting for connection ...")
 connection , address = s.accept() #metodo_accept_per_accettare_e_stabilre_connessione_con_il_client_restituendo_identificativo_connessione_e_IP_client
 print ('Client connected with address: ' , address)
 while 1: #scambio_dati_con_ciclo_while_infinito
-    data = connection.recv(1024) #utilizzato_per_ricevere_dati_client_a_1kb
+    data = random.randbytes(1024) #utilizzato_per_ricevere_dati_client_a_1kb
     if not data: break
     connection.sedall (b'-- Message <received --\n')
     print (data.decode ('utf-8')) #stampa_a_video_pacchetti_dopo_decodifica_a_utf-8
